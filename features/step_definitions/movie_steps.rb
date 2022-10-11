ff4d6129a0f16daea833ce6778c95d7254a6a210
Elizaveta Belkina <elizaveta.belkina@berkeley.edu>
2022-10-11 17:19:01 +0000

    assignment completed

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -16,7 +16,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  expect(page.body).to match(/#{e1}.*#{e2}/) 
+  expect(page.body).to match(/#{e1}[.\s\S]*#{e2}/) 
   # pending "Fill in this step in movie_steps.rb"
 end
 
